9dba174d870fb0b41f934dcdf15d0f2480e9ddad
AleksandarAleksandrov <amaleksandrov94@gmail.com>
2015-06-21 22:28:15 +0300

    database loser

--- com.cinematographer.core/src/main/java/com/cinematographer/core/movies/Reservation.java
+++ com.cinematographer.core/src/main/java/com/cinematographer/core/movies/Reservation.java
@@ -1,8 +1,47 @@
 package com.cinematographer.core.movies;
 
 import java.util.Collection;
-
+import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+@Entity
 public class Reservation {
+    
+        @Id
+        @NotNull
 	private Screening screening;
+        @OneToMany
 	private Collection<Seat> seats;
+
+    public Reservation()
+    {
+    }
+
+    public Reservation(Screening screening, Collection<Seat> seats)
+    {
+        this.screening = screening;
+        this.seats = seats;
+    }
+
+    public Screening getScreening()
+    {
+        return screening;
+    }
+
+    public Collection<Seat> getSeats()
+    {
+        return seats;
+    }
+
+    public void setScreening(Screening screening)
+    {
+        this.screening = screening;
+    }
+
+    public void setSeats(Collection<Seat> seats)
+    {
+        this.seats = seats;
+    }
+        
+        
 }
--- com.cinematographer.core/src/main/java/com/cinematographer/core/movies/Screening.java
+++ com.cinematographer.core/src/main/java/com/cinematographer/core/movies/Screening.java
@@ -2,11 +2,84 @@ package com.cinematographer.core.movies;
 
 import java.sql.Time;
 import java.util.Collection;
-
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+@Entity
+@Table(name = "SCREENING")
 public class Screening {
+        @Id
+        @NotNull
 	private String title;
 	private Time startTime;
 	private Time duration;
+        @OneToMany
 	private Collection<Seat> seats;
-	private int hall;
+	private Integer hall;
+
+    public Screening()
+    {
+    }
+
+    public Screening(String title, Time startTime, Time duration, Collection<Seat> seats, int hall)
+    {
+        this.title = title;
+        this.startTime = startTime;
+        this.duration = duration;
+        this.seats = seats;
+        this.hall = hall;
+    }
+
+    public Time getDuration()
+    {
+        return duration;
+    }
+
+    public int getHall()
+    {
+        return hall;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public Collection<Seat> getSeats()
+    {
+        return seats;
+    }
+
+    public Time getStartTime()
+    {
+        return startTime;
+    }
+
+    public void setDuration(Time duration)
+    {
+        this.duration = duration;
+    }
+
+    public void setHall(int hall)
+    {
+        this.hall = hall;
+    }
+
+    public void setSeats(Collection<Seat> seats)
+    {
+        this.seats = seats;
+    }
+
+    public void setStartTime(Time startTime)
+    {
+        this.startTime = startTime;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+        
+        
 }
