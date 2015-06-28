61b43dbb7fcf182d5bd70f8392a558b3c8da6eab
lub094 <lub094@hotmail.com>
2015-06-28 19:32:29 +0300

    Registered the User and the Reservation services in the bootstrap.

--- com.cinematographer.rest/src/main/java/com/cinematographer/rest/Bootstrap.java
+++ com.cinematographer.rest/src/main/java/com/cinematographer/rest/Bootstrap.java
@@ -4,13 +4,18 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.ws.rs.core.Application;
 
 import com.cinematographer.core.manager.IServiceManager;
 import com.cinematographer.core.manager.ServiceManager;
+import com.cinematographer.core.reservation.manager.IReservationService;
+import com.cinematographer.core.reservation.manager.ReservationService;
 import com.cinematographer.core.screening.service.IScreeningService;
 import com.cinematographer.core.screening.service.ScreeningService;
+import com.cinematographer.core.user.manager.IUserService;
+import com.cinematographer.core.user.manager.UserService;
 
 public class Bootstrap extends Application {
 
@@ -24,8 +29,11 @@ public class Bootstrap extends Application {
 	}
 
 	private void registerServices() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
 		IServiceManager manager = ServiceManager.getInstance();
-		manager.registerService(IScreeningService.class, new ScreeningService(
-				Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME)));
+
+		manager.registerService(IScreeningService.class, new ScreeningService(emf));
+		manager.registerService(IReservationService.class, new ReservationService(emf));
+		manager.registerService(IUserService.class, new UserService(emf));
 	}
 }
